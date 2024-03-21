d56152416666b396a8ea479205e78a3584151105
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2024-03-21 07:15:27 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello friends, welcome to my React + Vite + Tailwind CSS project
+      </h1>
+    </>
+  )
+}
+
+export default App
