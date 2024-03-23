bdab28aff637a5b13d9c7e609103273198d8c376
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2024-03-23 17:37:44 +0700

    build interface contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,27 +2,28 @@
 const Contact = () => {
   return (
     <>
-      <section className="relative flex flex-col-reverse md:flex-row items-center bg-[#0D182B]">
+      <section className="relative flex flex-col-reverse md:flex-row items-center bg-white">
         <div className="relative md:w-[35%]">
-          <div className="py-[30px] px-7 lg:px-14 md:py-14 w-full">
+          <div className="py-[40px] px-7 lg:px-14 md:py-14 w-full">
             <div className="relative">
-              <div className="w-1/2 border-b-[0px] sm:border-b-[1px] sm:border border-[#6A717C]"></div>
-              <div className="absolute top-[-1px] w-[15%] border-b-[0px] sm:border-b-[2px] sm:border-[#FF5256]"></div>
+              <div className="w-1/2 border-b-[0px] sm:border-b-[1px] sm:border border-[#53382c]"></div>
+              <div className="absolute top-[-1px] w-[15%] border-b-[0px] sm:border-b-[2px] sm:border-[#b22830]"></div>
             </div>
-            <h1 className="text-center sm:text-left font-primaryRegular text-white text-3xl md:text-3xl xl:text-[3rem] mb-5 mt-0 sm:mt-5 md:leading-tight">
-              Contact Us
+            <h1 className="uppercase text-center sm:text-left font-RobotoMedium text-[#53382c] hover:text-[#f1bc7a] text-3xl md:text-3xl xl:text-[3rem] mb-5 mt-0 sm:mt-5 md:leading-tight cursor-pointer">
+              Liên Hệ
             </h1>
-            <p className="font-TitilliumRegular text-center sm:text-left text-white text-[16px] lg:text-[18px] lg:w-[100%]">
-              Drop us an enquiry if you wish to learn more.
+            <p className="font-normal text-center sm:text-left text-black text-[16px] lg:text-[18px] lg:w-[100%]">
+              Highland Coffee Luôn Sẵn Sàng Lắng Nghe Và Hỗ Trợ Bạn
             </p>
           </div>
         </div>
-        <div className="md:w-[65%]">
+        <div className="relative md:w-[65%]">
           <img
-            src="https://www.gamudaland.com.my/_next/image?url=%2Fimages%2Fcontact%2Fcontact.png&w=1920&q=75"
-            alt="image"
-            className="w-full object-cover object-bottom h-[300px] lg:h-[500px]"
+            src="https://www.highlandscoffee.com.vn/vnt_upload/about/Highlands_5557_R3_-_Copy.jpg"
+            alt="banner"
+            className="w-full object-cover object-bottom h-full lg:h-[500px]"
           />
+          <div className="absolute inset-0 bg-gradient-to-l from-transparent to-white"></div>
         </div>
       </section>
 
@@ -30,49 +31,26 @@ const Contact = () => {
         <div className="w-full xl:w-10/12 mx-auto">
           <div className="grid grid-cols-12 gap-0 relative z-10">
             <div className="col-span-12 lg:col-span-7 relative z-10">
-              <iframe
-                src="https://snazzymaps.com/embed/493885"
-                className="w-full min-h-[400px] h-full"
-              ></iframe>
+              <div dangerouslySetInnerHTML={{
+                __html: `
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.5330909250315!2d106.7213798745749!3d10.770422059305398!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317525f5f3c420d3%3A0xffe8026e14a677b8!2zMTIzLCAxMjUgTmd1eeG7hW4gQ8ahIFRo4bqhY2gsIEFuIEzhu6NpIMSQw7RuZywgUXXhuq1uIDIsIFRow6BuaCBwaOG7kSBI4buTIENow60gTWluaCwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1711188504055!5m2!1svi!2s" width="710" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+                ` }} />
             </div>
-            <div className="col-span-12 lg:col-span-5 bg-[#0D182B] p-5 sm:p-10 lg:p-12 relative z-10">
-              <h1 className="leading-tight text-[#FFF] text-3xl md:text-3xl xl:text-[3rem] font-EBSemiBold relative">
-                Head Office
+            <div className="col-span-12 lg:col-span-5 bg-[#53382c] p-5 sm:p-10 lg:p-12 relative z-10">
+              <h1 className="leading-tight text-[#FFF] text-3xl md:text-3xl xl:text-[3rem] font-RobotoMedium relative">
+                Trụ Sở Chính
               </h1>
-              <p className="text-[14px] text-md font-bold text-[#FFF] my-6 font-TitilliumRegular">
-                Address
+              <p className="text-[14px] text-md font-bold text-[#FFF] my-6">
+                Địa Chỉ
               </p>
-              <p className="text-sm sm:text-base text-[#FFF] font-TitilliumRegular">
-                Menara Gamuda, PJ Trade Centre, No. 8, Jalan PJU 8/8A, Bandar
-                Damansara Perdana, 47820 Petaling Jaya, Selangor Darul Ehsan,
-                Malaysia.
+              <p className="text-sm sm:text-base text-[#FFF] font-semibold">
+                123-125 Nguyễn Cơ Thạch, Phường An Lợi Đông, Quận 2, Thành phố Hồ Chí Minh, Việt Nam.
               </p>
-              <div className="my-10">
-                <p className="text-[14px] text-md font-bold text-[#FFF] mb-3 font-TitilliumRegular">
-                  Get Direction
-                </p>
-                <div className="grid grid-cols-2 w-[20%] items-center">
-                  <a href="#" target="_blank">
-                    <img
-                      className="max-h-[20px] w-fit object-contain"
-                      src="https://www.gamudaland.com.my/images/contact/waze.svg"
-                      alt=""
-                    />
-                  </a>
-                  <a href="#" target="_blank">
-                    <img
-                      className="max-h-[20px] w-fit object-contain"
-                      src="https://www.gamudaland.com.my/images/contact/location.svg"
-                      alt=""
-                    />
-                  </a>
-                </div>
-              </div>
               <div className="my-6">
                 <div className="grid grid-cols-2 gap-3">
                   <div>
-                    <p className="text-[14px] text-md font-bold text-[#FFF] mb-3 font-TitilliumRegular">
-                      Contact Number
+                    <p className="text-[14px] text-md font-bold text-[#FFF] mb-3">
+                      Số Điện Thoại
                     </p>
                     <div className="flex flex-row items-center mb-3">
                       <img
@@ -81,10 +59,10 @@ const Contact = () => {
                         alt=""
                       />
                       <a
-                        className="text-[#FFF] font-TitilliumRegular text-sm sm:text-base"
+                        className="text-[#FFF] font-normal text-sm sm:text-base"
                         href="#"
                       >
-                        +603 7491 3200
+                        +028 6288 3966
                       </a>
                     </div>
                     <div className="flex flex-row items-center">
@@ -95,15 +73,15 @@ const Contact = () => {
                       />
                       <a
                         href="#"
-                        className="text-[#FFF] font-TitilliumRegular text-sm sm:text-base"
+                        className="text-[#FFF] font-normal text-sm sm:text-base"
                       >
-                        +603 7726 7646
+                        +028 6288 3966
                       </a>
                     </div>
                   </div>
                   <div>
-                    <p className="text-[14px] text-md font-bold text-[#FFF] mb-3 font-TitilliumRegular">
-                      Working Hours
+                    <p className="text-[14px] text-md font-bold text-[#FFF] mb-3">
+                      Giờ Làm Việc
                     </p>
                     <div className="flex flex-row items-center mb-3">
                       <img
@@ -111,7 +89,7 @@ const Contact = () => {
                         src="https://www.gamudaland.com.my/_next/image?url=%2Fimages%2Fcontact%2Fdatetime.png&w=32&q=75"
                         alt="#"
                       />
-                      <p className="text-sm sm:text-base text-[#FFF] font-TitilliumRegular">
+                      <p className="text-sm sm:text-base text-[#FFF] font-normal">
                         Mon - Fri 10am - 7pm
                       </p>
                     </div>
@@ -121,7 +99,7 @@ const Contact = () => {
                         src="https://www.gamudaland.com.my/_next/image?url=%2Fimages%2Fcontact%2Fdatetime.png&w=32&q=75"
                         alt="#"
                       />
-                      <p className="text-sm sm:text-base text-[#FFF] font-TitilliumRegular">
+                      <p className="text-sm sm:text-base text-[#FFF] font-normal">
                         Sat - Sun 10am - 5pm
                       </p>
                     </div>
@@ -136,59 +114,37 @@ const Contact = () => {
       <section className="relative bg-[#FFF] pt-[50px] lg:pt-[15rem] pb-[50px] lg:pb-[12rem] px-4 md:px-8 lg:-mt-44">
         <div className="mb-5">
           <div className="w-full">
-            <h1 className="text-3xl md:text-4xl lg:text-5xl xl:text-[3rem] text-[#091C34] text-center font-EBSemiBold w-full mx-auto title relative pb-5">
-              Enquiry
+            <h1 className="text-3xl md:text-4xl lg:text-5xl xl:text-[3rem] text-[#53382c] text-center font-RobotoMedium w-full mx-auto title relative pb-5">
+              Đóng Góp Ý Kiến
             </h1>
           </div>
         </div>
         <div className="w-full sm:w-3/4 xl:w-2/4 mx-auto">
-          <form>
+          <form action="">
             <input
               type="text"
-              className="outline-none font-TitilliumRegular p-0 py-3 md:py-2 lg:py-4 w-full border-b-[1px] border-black text-black mb-5"
-              placeholder="Full Name"
+              className="outline-none font-normal p-0 py-3 md:py-2 lg:py-4 w-full border-b-[1px] border-black text-black mb-5"
+              placeholder="Họ Và Tên"
               required
               maxLength={100}
             />
             <input
               type="email"
-              className="outline-none font-TitilliumRegular p-0 py-3 md:py-2 lg:py-4 w-full border-b-[1px] border-black text-black mb-5"
+              className="outline-none font-normal p-0 py-3 md:py-2 lg:py-4 w-full border-b-[1px] border-black text-black mb-5"
               placeholder="Email"
               required
               maxLength={100}
             />
             <textarea
               type="text"
-              placeholder="Message"
-              className="outline-none font-TitilliumRegular p-0 py-3 md:py-2 lg:py-4 w-full border-b-[1px] border-black text-black mb-5"
+              placeholder="Ý Kiến Đóng Góp"
+              className="outline-none font-normal p-0 py-3 md:py-2 lg:py-4 w-full border-b-[1px] border-black text-black mb-5"
               rows="4"
             ></textarea>
-            <div className="mb-5 flex min-h-[1.5rem] items-center">
-              <input className="w-5 h-4" type="checkbox" />
-              <label className="text-black text-sm inline-block pl-[0.15rem] hover:cursor-pointer font-TitilliumRegular">
-                I have read, agreed and accepted the
-                <a
-                  href="/disclaimer"
-                  target="_blank"
-                  className="text-[#E32326]"
-                >
-                  {" "}
-                  disclaimer{" "}
-                </a>
-                and
-                <a
-                  href="/privacy-notice"
-                  target="_blank"
-                  className="text-[#E32326]"
-                >
-                  {" "}
-                  disclaimer{" "}
-                </a>
-              </label>
-            </div>
+
             <button className="flex justify-center mx-auto items-center">
-              <div className="rounded-lg p-5 py-3 w-full text-white bg-[#E32426] hover:bg-[#000] flex justify-center items-center transition ease-in delay-100">
-                Submit Now
+              <div className="rounded-lg p-5 py-3 w-full text-white bg-[#b22830] hover:bg-[#880E0E] flex justify-center items-center transition ease-in delay-75">
+                Xác Nhận
               </div>
             </button>
           </form>
