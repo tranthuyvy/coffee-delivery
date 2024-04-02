03f392bc2610291fab3c019545d3a71cf23c2587
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2024-04-02 22:28:35 +0700

    feat: statistics column chart by year

--- src/Admin/components/ColumnChartStatistics.jsx
+++ src/Admin/components/ColumnChartStatistics.jsx
@@ -1,25 +1,71 @@
-import { ResponsiveContainer, BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar } from "recharts"
-import { data } from "../../apis/mock-data"
+import { useEffect, useState } from "react";
+import { ResponsiveContainer, BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Bar } from "recharts";
 
 const ColumnChartStatistics = () => {
+  const [data, setData] = useState([]);
+  const [selectedYear, setSelectedYear] = useState(2024);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const token = 'eyJhbGciOiJIUzM4NCJ9.eyJpYXQiOjE3MTE5NDk5MjMsImV4cCI6MTcxMjU1NDcyMywidXNlcm5hbWUiOiIrODQzNzMxNjI1ODYifQ.YQ5OhYHOmIUvgO9-6vwQopE29YCrCxh8EMmZGz8Sl98HP8F3kr3BGoEkCLXNiX52'
+        const response = await fetch(`http://localhost:9999/api/statistic/year?year=${selectedYear}`, {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        });
+        const result = await response.json();
+        if (response.ok) {
+          const fullYearData = Array.from({ length: 12 }, (_, index) => {
+            const monthData = result.data.find(item => item.moth === index + 1);
+            return {
+              moth: index + 1,
+              total_price: monthData ? monthData.total_price : 0
+            };
+          });
+          setData(fullYearData);
+        } else {
+          console.error("Failed to fetch data:", result.message);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, [selectedYear]);
+
+  const handleYearChange = (event) => {
+    setSelectedYear(parseInt(event.target.value));
+  };
+
   return (
     <div className="h-[24rem] bg-white p-4 rounded-md border border-gray-200 flex flex-col flex-1">
-      <strong className="text-sub font-semibold">Transactions</strong>
+      <strong className="text-sub font-semibold">Thống kê doanh thu</strong>
+      <div className="flex justify-end items-start mt-3">
+        <label htmlFor="yearSelect" className="mr-2 font-RobotoMedium">Choose year:</label>
+        <select id="yearSelect" value={selectedYear} onChange={handleYearChange} className="rounded-md font-RobotoMedium focus:border-none">
+          <option value={2020}>2020</option>
+          <option value={2021}>2021</option>
+          <option value={2022}>2022</option>
+          <option value={2023}>2023</option>
+          <option value={2024}>2024</option>
+        </select>
+      </div>
       <div className="w-full mt-3 flex-1 text-xs">
         <ResponsiveContainer width="100%" height="100%">
           <BarChart width={500} height={300} data={data} margin={{ top: 20, right: 10, left: -10, bottom: 0 }}>
             <CartesianGrid strokeDasharray="3 3 0 0" vertical={false} />
-            <XAxis dataKey="name" />
+            <XAxis dataKey="moth" />
             <YAxis />
             <Tooltip />
-            <Legend />
-            <Bar dataKey="Income" fill="#880E0E" />
-            <Bar dataKey="Expense" fill="#3A241B" />
+            {/* <Legend /> */}
+            <Bar dataKey="total_price" fill="#880E0E" />
           </BarChart>
         </ResponsiveContainer>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ColumnChartStatistics
+export default ColumnChartStatistics;
