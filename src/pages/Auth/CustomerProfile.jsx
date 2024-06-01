e96a62a983d6755749490b36ee8aa601c02fd9ca
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2024-06-01 13:49:25 +0700

    feat: customer edit profile

--- src/pages/Auth/CustomerProfile.jsx
+++ src/pages/Auth/CustomerProfile.jsx
@@ -1,16 +1,72 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getUserProfileRequest } from "../../redux/actions/actions";
 import { getRank } from "../../constants/Rank";
+import axios from "axios";
+import { format, parseISO } from "date-fns";
 
 const CustomerProfile = () => {
   const dispatch = useDispatch();
+  const token = localStorage.getItem("token");
   const user = useSelector(state => state.user.user.data);
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [formData, setFormData] = useState({
+    address: "",
+    birthday: "",
+    cccd: "",
+    phone: "",
+    email: "",
+    firstname: "",
+    lastname: "",
+    tax_id: ""
+  });
 
   useEffect(() => {
     dispatch(getUserProfileRequest());
 
   }, [dispatch])
+
+  console.log("user", user);
+
+  useEffect(() => {
+    if (user) {
+      setFormData({
+        address: user.address,
+        birthday: user.birthday ? format(parseISO(user.birthday), 'yyyy-MM-dd') : "",
+        phone: user.phone,
+        cccd: user.cccd,
+        email: user.email,
+        firstname: user.firstname,
+        lastname: user.lastname,
+        tax_id: user.tax_id
+      });
+    }
+  }, [user]);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSave = async () => {
+
+    try {
+      const response = await axios.put(`http://localhost:9999/api/customer/${user.user.user_id}/update`, formData, {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      });
+      console.log("Profile updated:", response.data);
+      setIsEditing(false);
+      dispatch(getUserProfileRequest());
+    } catch (error) {
+      console.error("Error updating profile:", error);
+    }
+  };
 
   return (
     <>
@@ -49,67 +105,126 @@ const CustomerProfile = () => {
               <p className="text-primary font-RobotoMedium text-[14px] my-4">{user?.user?.role?.role_name}</p>
               <p className="text-primary font-RobotoMedium text-[14px] my-4">{user?.address}</p>
             </div>
-
           </div>
 
           <div className="text-center my-4">
-            <button className="items-center justify-center w-[200px] rounded-md text-primary border-primary border hover:bg-primary hover:text-white py-2.5 px-10 transition-colors">
-              Edit Profile
-            </button>
+            {!isEditing ? (
+              <button
+                className="items-center justify-center w-[200px] rounded-md text-primary border-primary border hover:bg-primary hover:text-white py-2.5 px-10 transition-colors"
+                onClick={() => setIsEditing(true)}
+              >
+                Edit Profile
+              </button>
+            ) : (
+              <button
+                className="items-center justify-center w-[200px] rounded-md text-primary border-primary border hover:bg-primary hover:text-white py-2.5 px-10 transition-colors"
+                onClick={handleSave}
+              >
+                Save
+              </button>
+            )}
           </div>
         </div>
 
         <div className="mb-5 relative md:w-[70%] border-primary border-[1px] shadow-lg rounded-xl p-5">
           <div className="py-[40px] px-7 lg:px-14 w-full">
-            <div className="flex justify-center">
-              <div className="flex w-full justify-between border-b-[1px] font-RobotoMedium">
-                <div className="flex-1 text-left">Họ và Tên</div>
-                <div className="flex-1 text-right">{user?.firstname} {user?.lastname}</div>
-              </div>
-            </div>
-
-            <div className="flex justify-center mt-5">
-              <div className="flex w-full justify-between border-b-[1px] font-RobotoMedium">
-                <div className="flex-1 text-left">Email</div>
-                <div className="flex-1 text-right">{user?.email}</div>
-              </div>
-            </div>
-
-            <div className="flex justify-center mt-5">
-              <div className="flex w-full justify-between border-b-[1px] font-RobotoMedium">
-                <div className="flex-1 text-left">Phone</div>
-                <div className="flex-1 text-right">{user?.phone}</div>
-              </div>
-            </div>
-
-            <div className="flex justify-center mt-5">
-              <div className="flex w-full justify-between border-b-[1px] font-RobotoMedium">
-                <div className="flex-1 text-left">Birthday</div>
-                <div className="flex-1 text-right">{(new Date(user?.birthday)).toLocaleDateString()}</div>
-              </div>
-            </div>
-
-            <div className="flex justify-center mt-5">
-              <div className="flex w-full justify-between border-b-[1px] font-RobotoMedium">
-                <div className="flex-1 text-left">CCCD</div>
-                <div className="flex-1 text-right">{user?.cccd}</div>
-              </div>
-            </div>
-
-            <div className="flex justify-center mt-5">
-              <div className="flex w-full justify-between border-b-[1px] font-RobotoMedium">
-                <div className="flex-1 text-left">Ngày tham gia</div>
-                <div className="flex-1 text-right">{(new Date(user?.created_at)).toLocaleDateString()}</div>
-              </div>
-            </div>
-
-            <div className="flex justify-center items-center mt-5">
-              <div className="flex w-full justify-between items-center border-b-[1px] font-RobotoMedium">
-                <div className="text-left">Thành viên hạng</div>
-                <div className="text-right">{getRank(user?.user?.points)}</div>
-              </div>
-            </div>
-
+            {isEditing ? (
+              <>
+                <div className="flex justify-center mt-5">
+                  <div className="flex w-full justify-between border-b-[1px] font-RobotoMedium">
+                    <div className="flex-1 text-left">Họ</div>
+                    <div className="flex-1 text-right">
+                      <input type="text" name="firstname" value={formData.firstname} onChange={handleInputChange} />
+                    </div>
+                  </div>
+                </div>
+                <div className="flex justify-center mt-5">
+                  <div className="flex w-full justify-between border-b-[1px] font-RobotoMedium">
+                    <div className="flex-1 text-left">Tên</div>
+                    <div className="flex-1 text-right">
+                      <input type="text" name="lastname" value={formData.lastname} onChange={handleInputChange} />
+                    </div>
+                  </div>
+                </div>
+                <div className="flex justify-center mt-5">
+                  <div className="flex w-full justify-between border-b-[1px] font-RobotoMedium">
+                    <div className="flex-1 text-left">Email</div>
+                    <div className="flex-1 text-right">
+                      <input type="text" name="email" value={formData.email} onChange={handleInputChange} />
+                    </div>
+                  </div>
+                </div>
+                <div className="flex justify-center mt-5">
+                  <div className="flex w-full justify-between border-b-[1px] font-RobotoMedium">
+                    <div className="flex-1 text-left">Phone</div>
+                    <div className="flex-1 text-right">
+                      <input type="text" name="phone" value={formData.phone} onChange={handleInputChange} />
+                    </div>
+                  </div>
+                </div>
+                <div className="flex justify-center mt-5">
+                  <div className="flex w-full justify-between border-b-[1px] font-RobotoMedium">
+                    <div className="flex-1 text-left">Birthday</div>
+                    <div className="flex-1 text-center ml-10">
+                      <input type="date" name="birthday" value={formData.birthday} onChange={handleInputChange} />
+                    </div>
+                  </div>
+                </div>
+                <div className="flex justify-center mt-5">
+                  <div className="flex w-full justify-between border-b-[1px] font-RobotoMedium">
+                    <div className="flex-1 text-left">CCCD</div>
+                    <div className="flex-1 text-right">
+                      <input type="text" name="cccd" value={formData.cccd} onChange={handleInputChange} />
+                    </div>
+                  </div>
+                </div>
+              </>
+            ) : (
+              <>
+                <div className="flex justify-center mt-5">
+                  <div className="flex w-full justify-between border-b-[1px] font-RobotoMedium">
+                    <div className="flex-1 text-left">Họ và Tên</div>
+                    <div className="flex-1 text-right">{user?.firstname} {user?.lastname}</div>
+                  </div>
+                </div>
+                <div className="flex justify-center mt-5">
+                  <div className="flex w-full justify-between border-b-[1px] font-RobotoMedium">
+                    <div className="flex-1 text-left">Email</div>
+                    <div className="flex-1 text-right">{user?.email}</div>
+                  </div>
+                </div>
+                <div className="flex justify-center mt-5">
+                  <div className="flex w-full justify-between border-b-[1px] font-RobotoMedium">
+                    <div className="flex-1 text-left">Phone</div>
+                    <div className="flex-1 text-right">{user?.phone}</div>
+                  </div>
+                </div>
+                <div className="flex justify-center mt-5">
+                  <div className="flex w-full justify-between border-b-[1px] font-RobotoMedium">
+                    <div className="flex-1 text-left">Birthday</div>
+                    <div className="flex-1 text-right">{(new Date(user?.birthday)).toLocaleDateString()}</div>
+                  </div>
+                </div>
+                <div className="flex justify-center mt-5">
+                  <div className="flex w-full justify-between border-b-[1px] font-RobotoMedium">
+                    <div className="flex-1 text-left">CCCD</div>
+                    <div className="flex-1 text-right">{user?.cccd}</div>
+                  </div>
+                </div>
+                <div className="flex justify-center mt-5">
+                  <div className="flex w-full justify-between border-b-[1px] font-RobotoMedium">
+                    <div className="flex-1 text-left">Ngày tham gia</div>
+                    <div className="flex-1 text-right">{(new Date(user?.created_at)).toLocaleDateString()}</div>
+                  </div>
+                </div>
+                <div className="flex justify-center items-center mt-5">
+                  <div className="flex w-full justify-between items-center border-b-[1px] font-RobotoMedium">
+                    <div className="text-left">Thành viên hạng</div>
+                    <div className="text-right">{getRank(user?.user?.points)}</div>
+                  </div>
+                </div>
+              </>
+            )}
           </div>
         </div>
       </section>
